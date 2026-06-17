8ea937bcd15232011cc11a57c00c8732d5b1e759
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-06-17 19:59:53 +0700

    KDN-PC-02: Update

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-	'name' => env('WPSP_APP_NAME', 'WPSP Framework - WordPress Starter Plugin'),
+	'name' => env('WPSP_APP_NAME', 'WPSP Framework - WordPress Starter Plugin - Vue'),
 
 	/*
 	|--------------------------------------------------------------------------
